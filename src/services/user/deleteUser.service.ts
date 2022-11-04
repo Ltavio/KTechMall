b112467315f604c7fe11764a52a3dadcbf94671b
Ygor José <98772319+ygorjg@users.noreply.github.com>
2022-11-04 20:24:50 -0300

    feat: create user crud

---
+++ src/services/user/deleteUser.service.ts
@@ -0,0 +1,19 @@
+import AppDataSource from "../../data-source";
+import { User } from "../../entities/user.entity";
+import { AppError } from "../../errors/appErrors";
+
+const deleteUserService = async (id: string): Promise<boolean> => {
+  const userRepository = AppDataSource.getRepository(User);
+  const account = await userRepository.findOneBy({ id });
+
+  if (!account?.isActive) {
+    throw new AppError("Unable to delete inactive user");
+  }
+
+  account.isActive = false;
+  await userRepository.save(account);
+
+  return true;
+};
+
+export default deleteUserService;
