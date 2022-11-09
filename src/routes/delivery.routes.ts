9a7bd38c070b8f6fdb2d33854096fa6e66fc5ebb
Ygor José <98772319+ygorjg@users.noreply.github.com>
2022-11-09 12:02:20 -0300

    feat: delivery

---
+++ src/routes/delivery.routes.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import {
+  createDeliveryController,
+  updateDeliveryController,
+} from "../controllers/delivery.controller";
+
+import authTokenMiddleware from "../middlewares/authToken.middleware";
+
+const routes = Router();
+
+const deliveryRoutes = () => {
+  routes.post("/", authTokenMiddleware, createDeliveryController);
+  routes.patch("/:id", authTokenMiddleware, updateDeliveryController);
+  return routes;
+};
+
+export default deliveryRoutes;
