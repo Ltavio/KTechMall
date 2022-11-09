af71ab87126b1b3cdd86497cf9fad4b07f048472
Ygor José <98772319+ygorjg@users.noreply.github.com>
2022-11-09 12:00:58 -0300

    feat: delivery

---
+++ src/entities/delivery.entity.ts
@@ -0,0 +1,36 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToOne,
+  JoinColumn,
+} from "typeorm";
+
+@Entity("delivery")
+export default class Delivery {
+  @PrimaryGeneratedColumn("uuid")
+  readonly id: string;
+
+  @Column({ length: 100 })
+  address_id: string;
+
+  @Column({ default: true })
+  delivery: boolean;
+
+  @Column({ length: 100 })
+  receiver: string;
+
+  @CreateDateColumn({ type: "date" })
+  readonly createdAt: Date;
+
+  @UpdateDateColumn({ type: "date" })
+  readonly updatedAt: Date;
+
+  @OneToOne(() => Adress, {
+    eager: true,
+  })
+  @JoinColumn()
+  adress: Adress;
+}
