77c754f0d34cee0f830b7a66d6c94b399460dfef
Murillo Miguel <mmiguel.skn@gmail.com>
2022-11-08 22:12:43 -0300

    test:orders_product integration

---
+++ src/__test__/mocks/categories/index.ts
@@ -0,0 +1,6 @@
+import { ICategoryRequest } from "../../../interfaces/categories";
+
+export const mockedCategoryRequest: ICategoryRequest = {
+  name: "KTechMall Category",
+  description: "category description to KTechMall",
+};
---
+++ src/__test__/mocks/products/index.ts
@@ -0,0 +1,18 @@
+import { IProductRequest } from "../../../interfaces/products";
+
+export const mockedProductRequest: IProductRequest = {
+  description: "Mocked product to test",
+  name: "Mocked Product",
+  price: 13,
+  categoryId: "",
+  sellerId: "",
+  stock: 2
+};
+export const mockedSecondProductRequest: IProductRequest = {
+  description: "Other product Mocked to test",
+  name: "Other product Mocked",
+  price: 13,
+  categoryId: "",
+  sellerId: "",
+  stock: 1
+};
