033c6190283e741ca050e36ec279aced88288825
Lucas Mendes <lucasilveiramendes@gmail.com>
2022-11-09 22:06:22 -0300

    feat: add relation for address and delivery

--- src/entities/addresses.entity.ts
+++ src/entities/addresses.entity.ts
@@ -6,8 +6,10 @@ import {
   UpdateDateColumn,
   ManyToMany,
   JoinTable,
+  OneToMany,
 } from "typeorm";
 import { v4 as uuid } from "uuid";
+import Delivery from "./delivery.entity";
 import User from "./user.entity";
 
 @Entity("Addresses")
@@ -47,6 +49,9 @@ export default class Addresses {
   })
   @JoinTable()
   user: User[];
+
+  @OneToMany(() => Delivery, (Delivery) => Delivery.adress)
+  Delivery: Delivery[];
 
   constructor() {
     if (!this.id) {
